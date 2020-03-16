b34840fcc7d184b9699a6510651b05e380a9d44f
duongph <duongph@vnext.com.vn>
2020-03-16 18:19:18 +0700

    Update code

--- app/screens/boarding/OnBoardingScreen.js
+++ app/screens/boarding/OnBoardingScreen.js
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
 
 import React, { useState, useRef } from "react";
 import { View, Dimensions, StyleSheet, ImageBackground, TouchableOpacity, Image } from "react-native";
 import Text from '../../component/Text';
 import { scale, moderateScale } from '../../libs/reactSizeMatter/scalingUtils'
 import Swiper from 'react-native-swiper'
-=======
-import React, {useState, useRef} from 'react';
-import {
-  View,
-  Dimensions,
-  StyleSheet,
-  ImageBackground,
-  TouchableOpacity,
-} from 'react-native';
-import Carousel, {Pagination} from 'react-native-snap-carousel';
-import Text from '../../component/Text';
-import {scale, moderateScale} from '../../libs/reactSizeMatter/scalingUtils';
-import {SvgXml, SvgCss} from 'react-native-svg';
-import {CommonColors, CommonStyles} from '../../utils/CommonStyles';
-import ScaledSheet from '../../libs/reactSizeMatter/ScaledSheet';
->>>>>>> b115b26dbc001471b4b01efc610df2f26da930c9
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
@@ -48,7 +31,6 @@ const OnBoardingScreen = () => {
     },
   ];
 
-<<<<<<< HEAD
 return (
       <ImageBackground 
         source={require('../../../assets/images/bg_board.png')}
@@ -85,112 +67,6 @@ return (
           </Swiper>
       </ImageBackground>
 )
-=======
-  var slides = [];
-
-  const entriesSplitter = () => {
-    let size = 1; //Based on the size you want
-    while (entries.length > 0) {
-      slides.push(entries.splice(0, size));
-    }
-  };
-
-  // render every single slide
-  const _renderItem = ({item, index}) => {
-    return (
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
-        {item.map(item => {
-          return (
-            <ImageBackground
-              key={index}
-              source={require('../../../assets/images/bg_board.png')}
-              style={styles.backgroundImage}>
-              <View style={{flex: 1}}>
-                <View
-                  style={{
-                    flex: 3,
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <Text style={styles.textTop}>{item.title}</Text>
-                </View>
-
-                <View
-                  style={{
-                    flex: 3,
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <Text style={styles.textCenter}>{item.header}</Text>
-                  <Text style={styles.textContentCenter}>{item.content}</Text>
-                </View>
-
-                <View
-                  style={{
-                    bottom: -50,
-                    flex: 3,
-                    alignItems: 'center',
-                    alignContent: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <TouchableOpacity
-                    style={styles.buttonStart}
-                    onPress={() => {}}>
-                    <Text style={styles.textStart}>Get started</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </ImageBackground>
-          );
-        })}
-      </View>
-    );
-  };
-
-  return (
-    <View style={{paddingTop: 0}}>
-      {entriesSplitter()}
-      <Carousel
-        ref={carousel}
-        data={slides}
-        renderItem={_renderItem}
-        onSnapToItem={index => setActiveSlide(index)}
-        sliderWidth={screenWidth}
-        sliderHeight={screenHeight}
-        itemWidth={screenWidth}
-        autoplay={true}
-        autoplayInterval={2000}
-        loop={true}
-      />
-      <View
-        style={{
-          flex: 1,
-          position: 'absolute',
-          bottom: 0,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}>
-        <View style={{flex: 3}} />
-        <View style={{flex: 3}}>
-          <Pagination
-            dotsLength={3} // also based on number of sildes you want
-            activeDotIndex={activeSlide}
-            containerStyle={{backgroundColor: '', borderWidth: 0}}
-            dotStyle={styles.dotStyle}
-            inactiveDotStyle={{
-              backgroundColor: '#000',
-            }}
-            inactiveDotOpacity={0.65}
-            inactiveDotScale={0.6}
-          />
-        </View>
-        <View style={{flex: 3}} />
-      </View>
-    </View>
-  );
->>>>>>> b115b26dbc001471b4b01efc610df2f26da930c9
 };
 
 export default OnBoardingScreen;
@@ -236,16 +112,5 @@ const styles = StyleSheet.create({
     color: '#1A2C3C',
     textAlign: 'center',
   },
-<<<<<<< HEAD
   
 });
-=======
-  dotStyle: {
-    width: scale(14),
-    height: scale(14),
-    borderRadius: scale(7),
-    marginHorizontal: 0,
-    backgroundColor: '#FFF',
-  },
-});
->>>>>>> b115b26dbc001471b4b01efc610df2f26da930c9
